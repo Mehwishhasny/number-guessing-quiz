ea3970a603534bf6a9db817841c5a4e396d35a9e
Mehwish Hasny <mehwish.hasny@gmail.com>
2024-03-23 12:52:34 +0500

    Push my repo on git hub

---
+++ index.js
@@ -0,0 +1,11 @@
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+const answers = await inquirer.prompt([
+    { message: "Please guess a number between 1-6:", type: "number", name: "userGuessedNum" }
+]);
+if (answers.userGuessedNum === randomNumber) {
+    console.log("Congratulations! Your guess is right.");
+}
+else {
+    console.log("Oops! Your guess is too low. Please try again.");
+}
